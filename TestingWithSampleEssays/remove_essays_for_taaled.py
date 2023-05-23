0b2873e5eb5e0c72104798b9aa04f1c03934cd31
Varun Krishnan <2017.vkrishnan@gmail.com>
2023-05-23 16:44:33 -0400

    Added Sample Essays TAALED by removing files with < 50 words

---
+++ TestingWithSampleEssays/remove_essays_for_taaled.py
@@ -0,0 +1,33 @@
+""" 
+This script removes files that have less than 50 words so they are compatible for TAALED to run on 
+"""
+import os
+import shutil
+
+
+def count_words(file_path):
+    with open(file_path, "r") as file:
+        content = file.read()
+        words = content.split()
+        return len(words)
+
+
+def check_folder_files(folder_path, remove_folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(folder_path, filename)
+            num_words = count_words(file_path)
+            if num_words < 50:
+                print(
+                    f"File '{filename}' has less than 50 words. Removing from '{remove_folder_path}'."
+                )
+                remove_file_path = os.path.join(remove_folder_path, filename)
+                if os.path.exists(remove_file_path):
+                    os.remove(remove_file_path)
+
+
+folder_path = "SampleEssays/"  # Specify the path to the folder with .txt files
+remove_folder_path = (
+    "SampleEssaysTAALED/"  # Specify the path to the folder to remove files from
+)
+check_folder_files(folder_path, remove_folder_path)
